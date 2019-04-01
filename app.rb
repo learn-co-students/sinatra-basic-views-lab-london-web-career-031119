078a8bb1e44f2c96a9b94e1885947b6d8df619a1
Umar--Khan <umarkhandev@gmail.com>
2019-04-01 16:47:07 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    #! To link index page to html page. Which is created in the view folder called 'index.erb'
     get '/' do
       erb :index
     end
